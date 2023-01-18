9c25a8384c85186b77d28cb9b541c19616123f55
Magali Lopez Barrios <maguilopezbarrios1989@gmail.com>
2023-01-18 01:01:55 -0300

    Agregue objetos

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ switch (registro){
 // DECLARAR CANTIDAD DE PERSONAS
 if (registro == "si"){
     let adultos = parseInt(prompt("Cantidad de adultos"));
-let ninios = parseInt(prompt("Cantidad de niños"));
+    let ninios = parseInt(prompt("Cantidad de niños"));
 
-let totalPersonas = (adultos + ninios);
+    let totalPersonas = (adultos + ninios);
     if (totalPersonas <= 2){
-        alert("Puede elegir *monoambiente* o *2ambientes*");
+        alert("Puede elegir *monoambiente* o *dos ambientes*");
 
     }
     else if(totalPersonas = 3){
@@ -47,153 +47,79 @@ let totalPersonas = (adultos + ninios);
 else{
 
 }
-let adultos = parseInt(prompt("Cantidad de adultos"));
-let ninios = parseInt(prompt("Cantidad de niños"));
-
-let totalPersonas = (adultos + ninios);
-    if (totalPersonas <= 2){
-        alert("Puede elegir *monoambiente* o *2ambientes*");
-
-    }
-    else if(totalPersonas = 3){
-        alert ("Debe seleccionar un dpto *dos ambientes*");
-    }
-    else if(totalPersonas = 4){
-        alert ("Debe seleccionar un dpto *dos ambientes*");
-    }
-
-    else if (4 < totalPersonas <= 6){
-        alert("Puede elegir 1 *monoambiente y 1 dpto dos ambientes*; o *2 dptos dos ambientes*");
-    }
-    else if (6 < totalPersonas <= 8){
-        alert("Debe elegir *2 dptos dos ambientes*");
-    }
 
 
 // SELECCIONAR TIPO DE DEPARTAMENTO 
 //(boton monoambiente) 
-const monoambiente = 6000;
+/*const monoambiente = 6000;
 
 //(boton 2 ambientes)
-const dosAmbientes = 9500;
+const dosAmbientes = 9500;*/
 
 // BUSCAR FECHAS (presentar un calendario)
 
 
 // CANTIDAD DE NOCHES (como aun no se como se puede seleccionar fechas en calendario, voy a  solicitar "cantidad de noches")
+
+
+
+/*const nocheMonoambiente = 6000;
+const nocheDosAmbientes = 9500;*/
 let noches = parseInt(prompt("Ingrese cantidad de noches que se hospedará"));
 
 
-//CALCULAR COSTO DE ESTADIA
+
+//OBJETOS
+class departamento {
+    constructor (nombre, precio){
+        this.nombre = nombre,toUpperCase ();
+        this.precio = parseFloat (precio);
+        this.alquilado = false;
+    }
+}
+
+const departamento1 = new departamento ("monoambiente", "10000");
+const departamento2 = new departamento ("dosAmbientes", "12000");
+departamento1.precioEstadia ();
+departamento2.precioEstadia ();
+departamento1.alquilar ();
+
+console.log (departamento1.nombre)
+console.log (departamento1.precio)
+
+console.log (departamento2 ["nombre"])
+console.log (departamento2 ["precio"])
+
+//OPERADOR IN y FOR IN
+
+console.log ("nombre" in departamento1);
+
+for (const propiedad in departamento1){
+    console.log (departamento1) [propiedad];
+}
+
+//ARRAY
+const dptos = ["plantaBaja","primeroContrafrentre", "segundoFrente", "segundoContrafrente"];
+//acceso
+console.log (numeros [0])
+//recorrido
+const dptoss = ["plantaBaja","primeroContrafrentre", "segundoFrente", "segundoContrafrente"];
+for (let index = 0; index <3; index++){
+    alert (dptoss = [index]);
+    
+}
+/*//CALCULAR COSTO DE ESTADIA
 
 let precioEstadia1 = 0;
 let precioEstadia2 = 0;
 function multiplicar (noches, monoambiente, dosAmbientes){
-    precioEstadia1 = noche * monoambiente;
+    precioEstadia1 = noches * monoambiente;
     alert("El precio de su estadia es de " + precioEstadia1)
-    precioEstadia2 = noche * dosAmbientes;
+    precioEstadia2 = noches * dosAmbientes;
     alert("El precio de su estadia es de " + precioEstadia2)
+}*/
 
-}
-
-// REGISTRARSE
-const registro = prompt("¿Desea registrarse?");
-
-switch (registro){
-    case"si":
-        alert("Bienvenido, comencemos tu registro!!")
-        let nombre = prompt("Ingrese su nombre");
-        let apellido = prompt("Apellido");
-        let mail = parseInt(prompt("Mail"));
-
-        break;
-    case "no":
-        alert("Muchas gracias por visitarnos");
-        break;
-    default:
-        alert("Debe indicar si o no");
-        break;
-
-}
-
-// ***** BUSCAR DISPONIBILIDAD *****
-// DECLARAR CANTIDAD DE PERSONAS
-if (registro == "si"){
-    let adultos = parseInt(prompt("Cantidad de adultos"));
-let ninios = parseInt(prompt("Cantidad de niños"));
-
-let totalPersonas = (adultos + ninios);
-    if (totalPersonas <= 2){
-        alert("Puede elegir *monoambiente* o *2ambientes*");
-
-    }
-    else if(totalPersonas = 3){
-        alert ("Debe seleccionar un dpto *dos ambientes*");
-    }
-    else if(totalPersonas = 4){
-        alert ("Debe seleccionar un dpto *dos ambientes*");
-    }
-
-    else if (4 < totalPersonas <= 6){
-        alert("Puede elegir 1 *monoambiente y 1 dpto dos ambientes*; o *2 dptos dos ambientes*");
-    }
-    else if (6 < totalPersonas <= 8){
-        alert("Debe elegir *2 dptos dos ambientes*");
-    }
-
-}
-else{
-
-}
-let adultos = parseInt(prompt("Cantidad de adultos"));
-let ninios = parseInt(prompt("Cantidad de niños"));
-
-let totalPersonas = (adultos + ninios);
-    if (totalPersonas <= 2){
-        alert("Puede elegir *monoambiente* o *2ambientes*");
-
-    }
-    else if(totalPersonas = 3){
-        alert ("Debe seleccionar un dpto *dos ambientes*");
-    }
-    else if(totalPersonas = 4){
-        alert ("Debe seleccionar un dpto *dos ambientes*");
-    }
-
-    else if (4 < totalPersonas <= 6){
-        alert("Puede elegir 1 *monoambiente y 1 dpto dos ambientes*; o *2 dptos dos ambientes*");
-    }
-    else if (6 < totalPersonas <= 8){
-        alert("Debe elegir *2 dptos dos ambientes*");
-    }
-
-
-// SELECCIONAR TIPO DE DEPARTAMENTO 
-//(boton monoambiente) 
-const monoambiente = 6000;
-
-//(boton 2 ambientes)
-const dosAmbientes = 9500;
-
-// BUSCAR FECHAS (presentar un calendario)
-
-
-// CANTIDAD DE NOCHES (como aun no se como se puede seleccionar fechas en calendario, voy a  solicitar "cantidad de noches")
-let noches = parseInt(prompt("Ingrese cantidad de noches que se hospedará"));
-
-
-//CALCULAR COSTO DE ESTADIA
-
-let precioEstadia1 = 0;
-let precioEstadia2 = 0;
-function multiplicar (noches, monoambiente, dosAmbientes){
-    precioEstadia1 = noche * monoambiente;
-    alert("El precio de su estadia es de " + precioEstadia1)
-    precioEstadia2 = noche * dosAmbientes;
-    alert("El precio de su estadia es de " + precioEstadia2)
-
-    }
-
+   //FORMULARIO PAGINA "REGISTRARSE"
 let form=[];
 let submitbutton=document.getElementById("submitInput");
 let todook=1;
